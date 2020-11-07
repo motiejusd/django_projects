4cace2d450508feb1996487b6acdc30f47d57a29
motiejusd <motiejus.dauksa@gmail.com>
2020-11-07 14:05:28 +0200

    fixed models.py Task model, registered model in admin.py

--- todolist/models.py
+++ todolist/models.py
@@ -3,21 +3,14 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
-class Status(models.Model):
-    value = models.BooleanField(default=False)
-
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.value
-
-
 class Task(models.Model):
     description = models.CharField(
             max_length=200,
             help_text='Describe your task',
             validators=[MinLengthValidator(5, "Too short description")]
     )
-    status = models.ForeignKey('Status', on_delete=models.CASCADE, null=False)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """String for representing the Model object."""
