ab74366a2a796dc67816c415ba876c23563af120
motiejusd <motiejus.dauksa@gmail.com>
2020-11-09 12:09:51 +0200

    added basic bootstrap styling

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
 from todolist.models import Task
+from django import forms
 
 
 class MakeForm(ModelForm):
+    description = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Add task here',
+            }
+        )
+    )
+
     class Meta:
         model = Task
         fields = '__all__'
